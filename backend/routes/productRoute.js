ed8d6a5c1315254cde16d54fef1597289f5c0514
Himanshu Shukla <hshukla2611@gmail.com>
2022-07-07 23:47:49 +0530

    add api to get single product's details

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const { createProduct, getAllProducts, updateProduct, deleteProduct } = require('../controllers/productController.js');
+const { createProduct, getAllProducts, getProductDetails, updateProduct, deleteProduct } = require('../controllers/productController.js');
 
 const router = express.Router();
 
 // All the routes below are after '/api/v1/...'
 router.route('/product/new').post(createProduct);
 router.route('/products').get(getAllProducts);
-router.route('/product/:id').put(updateProduct);
-router.route('/product/:id').delete(deleteProduct);
+router.route('/product/:id')
+    .put(updateProduct)
+    .delete(deleteProduct)
+    .get(getProductDetails);
 
 module.exports = router;
