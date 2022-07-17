2fd361ff5700b976ac9d14907d72860027d333a7
Himanshu Shukla <hshukla2611@gmail.com>
2022-07-17 20:57:28 +0530

    add some error handling middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const errorMiddleware = require('./middlewares/error.js');
 
 app.use(express.json());
 
@@ -9,5 +10,8 @@ const product = require('./routes/productRoute.js');
 // Route definitions
 // Below URI will be used as the http://localhost:port/...
 app.use('/api/v1', product);
+
+// Middleware for error
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,4 +1,6 @@
 const Product = require('../models/productModel.js');
+const ErrorHander = require('../utils/errorHander.js');
+cosnt catchAsyncErrors = require('../middlewares/catchAsyncErrors.js');
 
 // Create product ---> Admin Route
 exports.createProduct = async(req, res, next) => {
@@ -10,7 +12,7 @@ exports.createProduct = async(req, res, next) => {
 };
 
 // Get all products
-exports.getAllProducts = async (req, res) => {
+exports.getAllProducts = async (req, res, next) => {
     const products = await Product.find();
     res.status(200).json({
         success: true,
@@ -19,13 +21,11 @@ exports.getAllProducts = async (req, res) => {
 };
 
 // Get product details
-exports.getProductDetails = async(req, res) => {
+exports.getProductDetails = async(req, res, next) => {
     const product = await Product.findById(req.params.id);
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product cannot be found"
-        });
+        // Calling error handler class so that we don't have to type it manually more than once
+        return next(new ErrorHander("Product not found", 404));
     }
     return res.status(200).json({
         success: true,
@@ -37,10 +37,8 @@ exports.getProductDetails = async(req, res) => {
 exports.updateProduct = async(req, res, next) => {
     const product = await Product.findByIdAndUpdate(req.params.id, req.body);
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product cannot be found"
-        });
+        // Calling error handler class so that we don't have to type it manually more than once
+        return next(new ErrorHander("Product not found", 404));
     } else{
         return res.status(200).json({
             success: true,
@@ -53,10 +51,8 @@ exports.updateProduct = async(req, res, next) => {
 exports.deleteProduct = async(req, res, next) => {
     const product = await Product.findByIdAndDelete(req.params.id);
     if (!product) {
-        return res.status(500).json({
-            success: false,
-            message: "Product cannot be found"
-        });
+        // Calling error handler class so that we don't have to type it manually more than once
+        return next(new ErrorHander("Product not found", 404));
     } else{
         return res.status(200).json({
             success: true,
